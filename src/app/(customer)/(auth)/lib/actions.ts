ceae644601464b6cb88f072919cb0aef0758f393
Fathur Muhammad <fatmuhdev@gmail.com>
2024-10-13 19:42:46 +0700

    Customer Sign In Page Slicing & Integrate

---
+++ src/app/(customer)/(auth)/lib/actions.ts
@@ -0,0 +1,58 @@
+"use server"
+
+import { schemaSignIn } from "@/lib/schema";
+import { ActionResult } from "@/types";
+import prisma from "../../../../../lib/prisma";
+import bcrypt from 'bcrypt'
+import {lucia} from "@/lib/auth";
+import {cookies} from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signIn(
+    _: unknown,
+    formData: FormData,
+): Promise<ActionResult> {
+
+    const validate = schemaSignIn.safeParse({
+            email: formData.get('email'),
+            password: formData.get('password')
+        }
+    );
+
+    if(!validate.success) {
+        return {
+            error: validate.error.errors[0].message,
+        }
+    }
+
+    const existingUser = await prisma.user.findFirst({
+       where: {
+           email: validate.data.email,
+           role: 'customer'
+       }
+    });
+
+    if(!existingUser) {
+        return {
+            error: 'Email Not Found'
+        }
+    }
+
+    const comparePassword = bcrypt.compareSync(validate.data.password, existingUser.password);
+
+    if (!comparePassword) {
+        return {
+            error: 'Email / password wrong'
+        }
+    }
+
+    const session = await lucia.createSession(existingUser.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+    )
+
+    return redirect('/')
+}
