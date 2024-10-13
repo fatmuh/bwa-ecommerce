04c544eb3b8e65f05c03234b5e5dff1bd4ebc695
Fathur Muhammad <fatmuhdev@gmail.com>
2024-10-13 18:03:00 +0700

    Create list view order

--- src/app/(admin)/dashboard/(index)/_components/sidebar.tsx
+++ src/app/(admin)/dashboard/(index)/_components/sidebar.tsx
@@ -67,7 +67,7 @@ export default function Sidebar() {
                     <Tooltip>
                         <TooltipTrigger asChild>
                             <Link
-                                href="#"
+                                href="/dashboard/orders"
                                 className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
                             >
                                 <ShoppingCart className="h-5 w-5"/>
